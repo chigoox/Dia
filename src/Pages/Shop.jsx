fda16f3186961d57649d797f4e05edd388b902b0
chigoox <dikeemmanuel54@gmail.com>
2023-04-19 04:38:46 -0400

    new

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -59,21 +59,23 @@ function Shop() {
 
     return (
         <div className=' h-screen gap-40 md:grid-cols-2 grid-cols-1 lg:grid-cols-4 p-12 mt-12 grid grid-flow-row'>
-            {product.map((item) => {
+            {PRODUCTDATA && PRODUCTDATA.map((item) => {
                 return (
                     <div className='flex items-center flex-col'>
                         <div key={item.name} className='h-64 w-64'>
-                            <img src={LIGHT} className='' alt="" />
+                            <img src={images[0]} className='' alt="" />
                         </div>
                         <div className=''>
                             <p className='text-center text-6xl font-bold'>{item.name}</p>
                             <div className='flex gap-2 items-center justify-center'>
-                                <p className='text-center text-4xl font-bold'>{item.price}</p>
-                                <p className='text-center font-bold'>{item.OGPrice}</p>
+                                <p className='text-center text-4xl font-bold'>{item.metadata.price}</p>
+                                <p className='text-center font-bold'>{item.metadata.OGPrice}</p>
                             </div>
                         </div>
                         <div className='center mt-4'>
-                            <button onClick={() => { checkOut(item.priceID) }} className='h-20 w-64 rounded-lg bg-blue-600 hover:bg-blue-400 trans-slow text-3xl font-bold'>Buy</button>
+                            <button onClick={() => {
+                                checkOut(item.default_price)
+                            }} className='h-20 w-64 rounded-lg bg-blue-600 hover:bg-blue-400 trans-slow text-3xl font-bold'>Buy</button>
                         </div>
                     </div>
                 )
