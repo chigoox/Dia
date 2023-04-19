2a7d893639853dc61edba2e95e916c3e7ebd5652
chigoox <dikeemmanuel54@gmail.com>
2023-04-19 13:42:11 -0400

    as

--- netlify/functions/FetchProducts.js
+++ netlify/functions/FetchProducts.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_API_KEY/* 'sk_test_51MamPiLPNdUzkCF3xdRvn0nkLpOrsJFo1um4Z7e07FlQXH6T7HCHhRxYkVjkK2iPW61EMZKoDM0ml6YSdWmAPcEn00E3jb1Gcr' */, {
+const stripe = require('stripe')(process.env.STRIPE_SECRET_API_KEY, {
   apiVersion: '2020-08-27',
   appInfo: { // For sample support and debugging, not required for production:
     name: "stripe-samples/checkout-one-time-payments",
